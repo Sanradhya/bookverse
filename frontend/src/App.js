5ea4f5313fb7e6fb11b49711247453d25c73f60c
Sanradhya <139942857+Sanradhya@users.noreply.github.com>
2025-01-07 22:29:43 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AddBookPage from "./pages/AddBookPage";
+import EditBookPage from "./pages/EditBookPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/add-book" element={<AddBookPage />} />
+        <Route path="/edit-book/:id" element={<EditBookPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
