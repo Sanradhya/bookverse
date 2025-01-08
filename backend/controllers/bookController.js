890248efb65509bb1499c4563b9e9f3745a18a23
Sanradhya <139942857+Sanradhya@users.noreply.github.com>
2025-01-08 20:36:12 +0530

    Add files via upload

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,1 +1,15 @@
+const Book = require('../models/Book');
 
+const getBooks = async (req, res) => {
+  const books = await Book.find({});
+  res.json(books);
+};
+
+const addBook = async (req, res) => {
+  const { title, author, price, category } = req.body;
+  const book = new Book({ title, author, price, category });
+  const savedBook = await book.save();
+  res.json(savedBook);
+};
+
+module.exports = { getBooks, addBook };
---
+++ backend/controllers/userController.js
@@ -0,0 +1,38 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({ name, email, password });
+  if (user) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(400).send('Invalid user data');
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+};
+
+module.exports = { registerUser, loginUser };
