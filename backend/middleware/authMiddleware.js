cbf58998378abb9d4b4def68a6377b26af5e6f34
Sanradhya <139942857+Sanradhya@users.noreply.github.com>
2025-01-08 20:37:26 +0530

    Add files via upload

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,19 @@
+// backend/middleware/authMiddleware.js
 
+const jwt = require('jsonwebtoken');
+
+// Middleware to check if the user is authenticated
+const protect = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user; // Attach decoded user info to the request
+    next(); // Call next middleware or route handler
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
+
+module.exports = { protect };
