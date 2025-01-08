55f3ee68e4a8d1d513bcc00e2c777f2687430bf0
Sanradhya <139942857+Sanradhya@users.noreply.github.com>
2025-01-08 20:40:01 +0530

    Add files via upload

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getBooks, addBook } = require('../controllers/bookController');
+const { protect } = require('../middleware/authMiddleware'); // Make sure this is correctly imported
 
+// Route for fetching and adding books
+router.route('/').get(getBooks).post(protect, addBook);
+
+module.exports = router;
